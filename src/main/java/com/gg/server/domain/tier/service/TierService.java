a57779ff7b9e923d7dc69d228f53f7f23108f60c
sion99 <kmicety1@gmail.com>
2023-08-28 13:07:33 +0900

    [REFACTOR] Tier 배정 로직 수정

--- src/main/java/com/gg/server/domain/tier/service/TierService.java
+++ src/main/java/com/gg/server/domain/tier/service/TierService.java
@@ -22,29 +22,28 @@ public class TierService {
 
     public void updateAllTier() {
         Season season = seasonFindService.findCurrentSeason(LocalDateTime.now());
-//        Season season = seasonFindService.findSeasonById(1L);
         List<Rank> rankList = rankRepository.findAllBySeasonIdOrderByPppDesc(season.getId());
         Integer totalNumber = rankRepository.countAllBySeasonId(season.getId());
         List<Tier> tierList = tierRepository.findAll();
 
         int cnt = 0;
-        int top10percent = (int) (totalNumber * 0.1);
-        int top5percent = (int) (totalNumber * 0.05);
+        int top10percentPpp = rankList.get((int) (totalNumber * 0.1)).getPpp();
+        int top5percentPpp = rankList.get((int) (totalNumber * 0.05)).getPpp();
         for (Rank rank : rankList) {
-            if (cnt < top5percent) {
-                rank.setTier(tierList.get(5));
-                cnt += 1;
-            } else if (cnt < top10percent) {
-                rank.setTier(tierList.get(4));
-                cnt += 1;
-            } else if (rank.getPpp() > 1200) {
-                rank.setTier(tierList.get(3));
-            } else if (rank.getPpp() > 1100) {
-                rank.setTier(tierList.get(2));
-            } else if (rank.getPpp() > 1000) {
-                rank.setTier(tierList.get(1));
-            } else {
+            if (rank.getPpp() < 980) {
                 rank.setTier(tierList.get(0));
+            } else if (rank.getPpp() < 1020) {
+                rank.setTier(tierList.get(1));
+            } else if (rank.getPpp() < 1060) {
+                rank.setTier(tierList.get(2));
+            } else if (rank.getPpp() < 1100) {
+                rank.setTier(tierList.get(3));
+            } else if (rank.getPpp() > top10percentPpp && rank.getPpp() > 1100) {
+                rank.setTier(tierList.get(4));
+            } else if (rank.getPpp() > top5percentPpp && rank.getPpp() > 1100) {
+                rank.setTier(tierList.get(5));
+            } else {
+                rank.setTier(tierList.get(4));
             }
         }
     }
